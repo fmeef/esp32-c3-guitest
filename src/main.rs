0b2f12c97646c41a7a97025ca01a58f069ac6a94
Alex Ballmer <gnu3ra@riseup.net>
2022-07-05 00:07:48 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,107 @@
 use anyhow::Result;
 use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::mono_font::iso_8859_13::FONT_8X13_BOLD;
 use embedded_graphics::mono_font::iso_8859_14::FONT_8X13;
+use embedded_graphics::mono_font::iso_8859_3::FONT_7X13;
 use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::Point;
+use embedded_graphics::prelude::Primitive;
+use embedded_graphics::primitives::Circle;
+use embedded_graphics::primitives::PrimitiveStyle;
+use embedded_graphics::primitives::Triangle;
 use embedded_graphics::text::renderer::TextRenderer;
 use embedded_graphics::text::Text;
 use embedded_graphics::text::TextStyleBuilder;
 use embedded_graphics::Drawable;
 use embedded_hal::blocking::delay::DelayMs;
-use epd_waveshare::epd2in13_v2::Display2in13;
-use epd_waveshare::epd2in13bc::Display2in13bc;
-use epd_waveshare::epd2in13bc::Epd2in13bc;
+use embedded_hal::prelude::_embedded_hal_blocking_spi_Write;
+use embedded_hal::spi::Mode;
+use epd_waveshare::epd2in13_v3::Display2in13;
+use epd_waveshare::epd2in13_v3::Epd2in13;
 use epd_waveshare::graphics::Display;
-use epd_waveshare::prelude::TriColor;
+use epd_waveshare::prelude::Color;
 use epd_waveshare::prelude::WaveshareDisplay;
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::Gpio1;
 use esp_idf_hal::gpio::Gpio10;
+use esp_idf_hal::gpio::Gpio2;
+use esp_idf_hal::gpio::Gpio5;
+use esp_idf_hal::gpio::Gpio6;
 use esp_idf_hal::gpio::Gpio7;
+use esp_idf_hal::gpio::Input;
+use esp_idf_hal::gpio::Output;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::spi;
-use std::process::Output; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+fn setup_display() -> Result<()> {
+    Ok(())
+}
+
 fn main() -> Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
+    esp_idf_logger::init().expect("failed to setup logging");
     let peripherals = Peripherals::take().unwrap();
     let spi = peripherals.spi2;
 
     let mut delay = FreeRtos;
 
-    let sclk = peripherals.pins.gpio6;
-    let serial_out = peripherals.pins.gpio7;
+    println!("delay setup");
+
+    let sclk = peripherals.pins.gpio6.into_output()?;
+    let serial_out = peripherals.pins.gpio7.into_output()?;
     let cs = peripherals.pins.gpio10.into_output()?;
-    let dc = peripherals.pins.gpio1.into_output()?;
-    let busy = peripherals.pins.gpio3.into_input()?;
-    let rst = peripherals.pins.gpio2.into_output()?;
+    let dc = peripherals.pins.gpio8.into_output()?;
+    let busy = peripherals.pins.gpio1.into_input()?;
+    let rst = peripherals.pins.gpio0.into_output()?;
+    println!("gpio set");
+    let config = <spi::config::Config as Default>::default().baudrate(1.MHz().into());
 
-    let config = <spi::config::Config as Default>::default().baudrate(8.MHz().into());
-
+    println!("spi config");
     let mut spi = spi::Master::<spi::SPI2, _, _, _, _>::new(
         spi,
         spi::Pins {
             sclk,
             sdo: serial_out,
-            sdi: None::<Gpio7<Output>>,
-            cs: None::<Gpio10<Output>>,
+            sdi: None::<Gpio2<Input>>,
+            cs: None::<Gpio1<Output>>,
         },
         config,
     )?;
 
-    let mut driver = Epd2in13bc::new(&mut spi, cs, busy, dc, rst, &mut delay)?;
-    let mut buf = Display2in13::default();
+    println!("spi setup");
 
-    let style = MonoTextStyle::new(&FONT_8X13, BinaryColor::On);
+    let mut driver = Epd2in13::new(&mut spi, cs, busy, dc, rst, &mut delay).expect("cry");
+    let mut buf = Display2in13::default();
+    buf.set_rotation(epd_waveshare::graphics::DisplayRotation::Rotate90);
+    let style = MonoTextStyle::new(&FONT_8X13_BOLD, BinaryColor::On);
     Text::new(
         "Look up at a star\nits incredibly far\nbut further away altogether...",
-        Point::new(0, 0),
+        Point::new(0, 10),
         style,
     )
     .draw(&mut buf)?;
+
+    Triangle::new(Point::new(20, 60), Point::new(40, 60), Point::new(30, 75))
+        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+        .draw(&mut buf)?;
+
+    Triangle::new(Point::new(20, 70), Point::new(40, 70), Point::new(30, 55))
+        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+        .draw(&mut buf)?;
+
+    driver.set_refresh(
+        &mut spi,
+        &mut delay,
+        epd_waveshare::prelude::RefreshLut::Full,
+    )?;
+    driver.clear_frame(&mut spi, &mut delay)?;
+    driver.display_frame(&mut spi, &mut delay)?;
     driver.update_and_display_frame(&mut spi, buf.buffer(), &mut delay)?;
-    println!("Hello, world!");
     loop {
-        delay.delay_ms(1000 as u32);
+        driver.display_frame(&mut spi, &mut delay)?;
+        delay.delay_ms(1 as u32); // avoid tripping watchdog
     }
 }
