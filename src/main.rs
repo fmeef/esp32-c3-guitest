5dbf9d077154b8f402b1bd99cf52872892385137
Alex Ballmer <gnu3ra@riseup.net>
2022-11-29 00:47:27 -0600

    update epdwaveshare to new pr changes

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,27 @@
 use anyhow::Result;
-use embedded_graphics::draw_target::DrawTarget;
+
 use embedded_graphics::mono_font::iso_8859_13::FONT_8X13_BOLD;
-use embedded_graphics::mono_font::iso_8859_14::FONT_8X13;
-use embedded_graphics::mono_font::iso_8859_3::FONT_7X13;
+
 use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::pixelcolor::BinaryColor;
+
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::Circle;
+
 use embedded_graphics::primitives::PrimitiveStyle;
 use embedded_graphics::primitives::Triangle;
-use embedded_graphics::text::renderer::TextRenderer;
+
 use embedded_graphics::text::Text;
-use embedded_graphics::text::TextStyleBuilder;
+
 use embedded_graphics::Drawable;
-use embedded_hal::blocking::delay::DelayMs;
-use embedded_hal::prelude::_embedded_hal_blocking_spi_Write;
-use embedded_hal::spi::Mode;
+
 use epd_waveshare::epd2in13_v3::Display2in13;
 use epd_waveshare::epd2in13_v3::Epd2in13;
-use epd_waveshare::graphics::Display;
+
 use epd_waveshare::prelude::Color;
 use epd_waveshare::prelude::WaveshareDisplay;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::spi;
-
-fn setup_display() -> Result<()> {
-    Ok(())
-}
-
 fn main() -> Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
@@ -68,7 +60,7 @@ fn main() -> Result<()> {
     let mut driver = Epd2in13::new(&mut spi, cs, busy, dc, rst, &mut delay).expect("cry");
     let mut buf = Display2in13::default();
     buf.set_rotation(epd_waveshare::graphics::DisplayRotation::Rotate90);
-    let style = MonoTextStyle::new(&FONT_8X13_BOLD, BinaryColor::On);
+    let style = MonoTextStyle::new(&FONT_8X13_BOLD, Color::Black);
     Text::new(
         "Look up at a star\nits incredibly far\nbut further away altogether...\nI make a many tired noise\nzzz",
         Point::new(0, 10),
@@ -77,11 +69,11 @@ fn main() -> Result<()> {
     .draw(&mut buf)?;
 
     Triangle::new(Point::new(20, 60), Point::new(40, 60), Point::new(30, 75))
-        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+        .into_styled(PrimitiveStyle::with_fill(Color::Black))
         .draw(&mut buf)?;
 
     Triangle::new(Point::new(20, 70), Point::new(40, 70), Point::new(30, 55))
-        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+        .into_styled(PrimitiveStyle::with_fill(Color::Black))
         .draw(&mut buf)?;
 
     driver.set_refresh(
